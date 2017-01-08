55937a90c399cae623d0326c4ef6e64da93f10cb
Jeremy Fehr <jfehr7212@gmail.com>
2017-01-07 23:48:58 -0600

    Solution to 1.2

--- 01/solution.hs
+++ 01/solution.hs
@@ -1,10 +1,26 @@
-import qualified Data.Text as T
-
 main = do
   text <- readFile "input.txt"
-  putStrLn (show (numFloors (T.pack text)))
+  putStrLn ("Part one: " ++ (show (sumFloors text)))
+  putStrLn ("Part one: " ++ (show (firstFloorNegative text)))
 
-numFloors :: T.Text -> Int
-numFloors t = opens - closes
-  where opens = T.count (T.pack "(") t
-        closes = T.count (T.pack ")") t
+sumFloors :: [Char] -> Int
+sumFloors [] = 0
+sumFloors (x:xs) = if x == ')' then -1 + sumFloors xs
+                   else if x == '(' then 1 + sumFloors xs
+                   else sumFloors xs
+
+firstFloorNegative :: [Char] -> Int
+firstFloorNegative xs = _firstFloorNegative 0 0 xs
+
+_firstFloorNegative :: Int -> Int -> [Char] -> Int
+_firstFloorNegative pos sum [] = -1
+_firstFloorNegative pos sum (x:xs) =
+  if x == ')' then
+    if sum - 1 == -1 then
+      pos + 1
+    else
+      _firstFloorNegative (pos + 1) (sum - 1) xs
+  else if x == '(' then
+    _firstFloorNegative (pos + 1) (sum + 1) xs
+  else
+    _firstFloorNegative (pos + 1) sum xs
