52a5e307a2d8c0fe6f9fc6f8c4a0959d77d5a1ce
Jeremy Fehr <jfehr7212@gmail.com>
2017-01-31 11:47:15 -0600

    Solution to 2015.12

--- 12/solution.hs
+++ 12/solution.hs
@@ -1,10 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Test.HUnit
+
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.HashMap.Strict as M
+import qualified Data.Vector as V
+import Data.Aeson
 import Data.Char
+import Data.Scientific
 
 main = do
   runTestTT partonetests
   text <- readFile "input.txt"
   putStrLn $ "Part one: " ++ (show . partone $ text)
+  putStrLn $ "Part two: " ++ (show . parttwo $ text)
 
 partonetests = TestList [
   6 ~=? (partone "[1,2,3]"),
@@ -14,7 +23,8 @@ partonetests = TestList [
   0 ~=? (partone "[]"),
   0 ~=? (partone "{}")]
 
--- Sums all the numbers in the file
+-- I could rewrite this to use the parser from part two, but that's a tragic
+-- waste of time.
 partone :: String -> Int
 partone s = sumtext 0 "" s
 
@@ -32,3 +42,19 @@ sumtext tot seg _
       tot + read seg
   | otherwise =
       tot
+
+parttwo :: String -> Int
+parttwo s = countignoringred $ fromJust $ decode $ B.pack s
+
+-- Assumption: nothing but integer numbers
+countignoringred :: Value -> Int
+countignoringred (Object m)
+  | String "red" `elem` M.elems m = 0
+  | otherwise = sum $ map countignoringred $ M.elems m
+countignoringred (Array a) = sum $ map countignoringred $ V.toList a
+countignoringred (Number n) = fromJust $ toBoundedInteger n
+countignoringred _ = 0
+
+fromJust :: Maybe a -> a
+fromJust (Just x) = x
+fromJust _ = error "unsafe"
