cbc8e9556b4b1d925085f185e41b9eafc7901950
Jeremy Fehr <jfehr7212@gmail.com>
2017-02-03 17:44:39 -0600

    Solution to 2015.19

--- 19/solution.hs
+++ 19/solution.hs
@@ -1,3 +1,4 @@
+import Data.Char (isLower, isUpper)
 import Data.List
 import qualified Data.Map as M
 import Data.String.Utils (join, split)
@@ -8,9 +9,17 @@ main = do
   runTestTT partOneTests
   text <- readFile "input.txt"
   putStrLn $ "Part one: " ++ (show $ countMolecules (getRules text) (getMolecule text))
+  runTestTT partTwoTests
+  -- BFS implementation, too slow.
+  --putStrLn $ "Part two: " ++ (show $ generateMolecule (getRules text) (getMolecule text))
+  putStrLn $ "Part two: " ++ (show $ genMolecule (getMolecule text))
 
 partOneTests = TestList [
   4 ~=? (countMolecules [("H", "HO"), ("H", "OH"), ("O", "HH")] "HOH")]
+
+partTwoTests = TestList [
+  3 ~=? (generateMolecule [("e", "H"), ("e", "O"), ("H", "HO"), ("H", "OH"), ("O", "HH")] "HOH"),
+  6 ~=? (generateMolecule [("e", "H"), ("e", "O"), ("H", "HO"), ("H", "OH"), ("O", "HH")] "HOHOHO")]
 
 countMolecules :: [(String, String)] -> String -> Int
 countMolecules rules base =
@@ -34,3 +43,35 @@ getRules s =
 
 getMolecule :: String -> String
 getMolecule s = last $ lines s
+
+genMolecule :: String -> Int
+genMolecule s =
+  let atoms = (getAtoms s)
+      countAtoms atom = length $ filter (atom ==) (getAtoms s)
+  in length atoms - countAtoms "Rn" - countAtoms "Ar" - 2 * countAtoms "Y" - 1
+
+getAtoms :: String -> [String]
+getAtoms (x:y:rest)
+  | isUpper x && isLower y = ([x] ++ [y]) : (getAtoms rest)
+  | isUpper x && isUpper y = ([x]) : (getAtoms (y:rest))
+  | otherwise = error "bad molecule"
+getAtoms (x:rest)
+  | isUpper x = ([x]) : (getAtoms rest)
+  | otherwise = error "bad molecule"
+getAtoms "" = []
+
+-- BFS, too slow, but is correct
+generateMolecule :: [(String, String)] -> String -> Int
+generateMolecule rules target = fromJust $ findIndex (target `elem`) $ iterate (iter rules) ["e"]
+
+iter :: [(String, String)] -> [String] -> [String]
+iter rules bases = nub $ iter' rules bases
+  where
+    iter' :: [(String, String)] -> [String] -> [String]
+    iter' rules (base:rest) =
+      concatMap (flip replace base) rules ++ (iter' rules rest)
+    iter' _ _ = []
+
+fromJust :: Maybe a -> a
+fromJust (Just x) = x
+fromJust _ = error "unsafe"
