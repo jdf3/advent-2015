1871b2882272a124cdecdb625dcc3e4ad1a3d514
Jeremy Fehr <jfehr7212@gmail.com>
2017-02-03 12:40:53 -0600

    Solution to 2015.18

---
+++ 18/solution.hs
@@ -0,0 +1,57 @@
+import Test.HUnit
+import qualified Data.Map as M
+import Data.List
+
+main = do
+  runTestTT partOneTests
+  text <- readFile "input.txt"
+  putStrLn $ "Part one: " ++ (show $ countLights text 100)
+
+testBoard = ".#.#.#\n\
+            \...##.\n\
+            \#....#\n\
+            \..#...\n\
+            \#.#..#\n\
+            \####.."
+
+partOneTests = TestList [
+  11 ~=? (countLights testBoard 1),
+  8 ~=? (countLights testBoard 2),
+  4 ~=? (countLights testBoard 3),
+  4 ~=? (countLights testBoard 4)]
+
+data Light = On | Off deriving Eq
+
+countLights :: String -> Int -> Int
+countLights s i =
+  let lights = parseLights s
+      finish = (iterate iterLights lights) !! i
+  in length $ filter (== On) $ M.elems finish
+
+iterLights :: M.Map (Int, Int) Light -> M.Map (Int, Int) Light
+iterLights m = M.fromList $ map (\coord -> (coord, iterLight m coord)) $ M.keys m
+
+iterLight :: M.Map (Int, Int) Light -> (Int, Int) -> Light
+iterLight m (x, y) =
+  let light = m M.! (x, y)
+      ns = onNeighbors (x, y) m
+  in case (light, ns) of
+    (On, 2) -> On
+    (On, 3) -> On
+    (Off, 3) -> On
+    _ -> Off
+
+onNeighbors :: (Int, Int) -> M.Map (Int, Int) Light -> Int
+onNeighbors coord m = length $ filter (\x -> x == Just On) $ map (flip M.lookup m) $ neighbors coord
+
+neighbors :: (Int, Int) -> [(Int, Int)]
+neighbors (x, y) = [(a, b) | a <- [x-1 .. x+1], b <- [y-1 .. y+1]] \\ [(x, y)]
+
+parseLights :: String  -> M.Map (Int, Int) Light
+parseLights s = M.fromList $ getLights s 0 0
+
+getLights :: String -> Int -> Int -> [((Int, Int), Light)]
+getLights ('.':xs) x y = ((x, y), Off) : (getLights xs (x+1) y)
+getLights ('#':xs) x y = ((x, y), On) : (getLights xs (x+1) y)
+getLights ('\n':xs) x y = getLights xs 0 (y+1)
+getLights "" _ _ = []
